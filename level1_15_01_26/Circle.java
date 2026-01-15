6bf4ba8fe1cb68c9ad73bef7fde51d6ce8324089
khushi gupta <khushi512.be22@chitkara.edu.in>
2026-01-15 23:51:06 +0530

    Assignment done

---
+++ level1_15_01_26/Circle.java
@@ -0,0 +1,24 @@
+package level1_15_01_26;
+class Circle {
+
+    double radius;
+
+    void calculateArea() {
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+
+    void calculateCircumference() {
+        double circumference = 2 * 3.14 * radius;
+        System.out.println("Circumference of Circle: " + circumference);
+    }
+
+    public static void main(String[] args) {
+
+        Circle c = new Circle();
+        c.radius = 5;
+
+        c.calculateArea();
+        c.calculateCircumference();
+    }
+}
